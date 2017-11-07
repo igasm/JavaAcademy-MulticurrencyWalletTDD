017b89dc8c2e4d63c8cb25a9d2a5eb1c1b9dd060
iga <iga.smarz@gmail.com>
2017-11-07 09:58:18 +0100

    test for Dollar equality added

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,7 +6,17 @@ public class DollarTest {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(five.amount, 10);
+        Dollar product = five.times(2);
+        assertEquals(product.amount, 10);
+        product = five.times(3);
+        assertEquals(product.amount, 15);
     }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+
 }
